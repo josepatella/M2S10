909bac3a134187f0a66d37472f5cce725bdd06ac
Patella <jfpatella@gmail.com>
2023-07-31 21:52:22 -0300

    Initial commit

---
+++ src/main/java/com/example/M2S10/M2S10Application.java
@@ -0,0 +1,13 @@
+package com.example.M2S10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2S10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2S10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/M2S10/M2S10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.M2S10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2S10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
